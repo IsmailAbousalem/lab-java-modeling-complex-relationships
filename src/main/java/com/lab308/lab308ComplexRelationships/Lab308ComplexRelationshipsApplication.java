d27d8b71fe885e37930e12943c3790f9c00e2947
Ismail <ia.abousalem@gmail.com>
2024-06-27 14:36:32 -0400

    created spring project and cloned repo

---
+++ src/main/java/com/lab308/lab308ComplexRelationships/Lab308ComplexRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.lab308.lab308ComplexRelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab308ComplexRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab308ComplexRelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab308/lab308ComplexRelationships/Lab308ComplexRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab308.lab308ComplexRelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab308ComplexRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
